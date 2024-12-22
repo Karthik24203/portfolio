5b17509a8190e1fdef0419bcf89dd07b1016d0bb
Karthik V <karthikv24203@gmail.com>
2024-12-22 15:55:01 +0530

    3rd commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import ScrollText from "../components/ScrollText";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
-import Slider from "../components/slider";
 import Header from "../components/Header";
 import Stacks from "@/components/Stacks";
 
