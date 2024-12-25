779a305bb8f441b4aaff2f5e835cdb5d17bc22ab
Karthik V <karthikv24203@gmail.com>
2024-12-25 18:34:25 +0530

    proj - resp1

--- app/_components/CarouselSlide.jsx
+++ app/_components/CarouselSlide.jsx
@@ -19,9 +19,11 @@ const Carousel = () => {
   );
 
   const updateVisibleSlides = useCallback(() => {
-    if (window.innerWidth < 470) {
+    if (window.innerWidth < 420) {
+      setVisibleSlides(1.9);
+    } else if (window.innerWidth < 470) {
       setVisibleSlides(1.7);
-    } else if (window.innerWidth < 700) {
+    }  else if (window.innerWidth < 700) {
       setVisibleSlides(1.5);
     } else if (window.innerWidth < 956) {
       setVisibleSlides(2);
