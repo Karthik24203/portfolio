b908829b89ba18ead81d3461b2b6a2fcaa6cf3ee
Karthik V <karthikv24203@gmail.com>
2024-12-29 19:01:31 +0530

    exp

--- app/_components/About.jsx
+++ app/_components/About.jsx
@@ -46,15 +46,15 @@ const About = () => {
               experiences.
             </p>
             <div className=" mt-1">
-              <h2 className=" text-2xl font-semibold flex gap-3 items-center">
+              <h2 className=" text-2xl font-semibold flex gap-3 justify-center md:justify-start items-center">
                 Experience <LiaCertificateSolid size={30} className="" />
               </h2>
-              <div className=" border-black px-5 pb-5 pt-3 rounded-lg border-2">
+              <div className=" border-black px-5 pb-5 mt-2 pt-3 rounded-lg border-2">
                 <h3 className=" font-semibold text-xl">Intern</h3>
                 <p className=" font-semibold">
                   Master Control Facility, ISRO,Hassan
                 </p>
-                <p>
+                <p className=" md:text-lg">
                   I landed the role as intern to develop a web dev software to
                   analyze satellite readings related to its position and orbit
                   and generate an alert if any major fluctuations in the
